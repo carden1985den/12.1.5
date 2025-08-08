41c84f2f3cdc10924a11aebe170d25b2dc50fd9f
denis.tsaregorodtsev <carden1985@gmail.com>
2025-08-08 11:03:08 +0400

    Add project files.

---
+++ Advertisement.cs
@@ -0,0 +1,14 @@
+﻿class Advertisement
+{
+    public static void ShowAds()
+    {
+        Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+        Thread.Sleep(1000);
+
+        Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+        Thread.Sleep(2000);
+
+        Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+        Thread.Sleep(3000);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        List<User> usersList = new List<User>();
+        usersList.Add(new User() { Name = "Denis", Login = "den", IsPremium = true });
+        usersList.Add(new User() { Name = "Ivan", Login = "vak", IsPremium = false });
+        usersList.Add(new User() { Name = "Georg", Login = "Gore", IsPremium = false });
+
+        foreach (var user in usersList)
+        {
+            string helloMessage = $"Приветствую тебя {user.Name} ({user.Login})";
+            if (user.IsPremium == true)
+            {
+                Console.WriteLine(helloMessage);
+            }else 
+            {
+                Advertisement.ShowAds();
+                Console.WriteLine(helloMessage);
+            }
+        }
+    }
+}
